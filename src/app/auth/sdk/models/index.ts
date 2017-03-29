e8970a413fa0e081b56734d69c97a4e0754f4f0d
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2017-03-29 15:23:15 -0300

    Change for validate email

--- src/app/auth/sdk/models/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-export * from './Email';
-export * from './Person';
-export * from './Pet';
-export * from './BaseModels';
-
--- src/app/auth/sdk/services/custom/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-export * from './Email';
-export * from './Person';
-export * from './Pet';
-export * from './SDKModels';
-export * from './logger.service';
